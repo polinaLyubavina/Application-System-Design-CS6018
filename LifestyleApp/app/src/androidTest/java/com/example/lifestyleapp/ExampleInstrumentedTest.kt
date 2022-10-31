46b3f31ceb3576213db51071120960a7a975381f
polinaLyubavina <polinalyub@tutanota.com>
2022-10-31 10:35:58 -0600

    Add functioning automated tests

--- LifestyleApp/app/src/androidTest/java/com/example/lifestyleapp/ExampleInstrumentedTest.kt
+++ LifestyleApp/app/src/androidTest/java/com/example/lifestyleapp/ExampleInstrumentedTest.kt
@@ -1,5 +1,7 @@
 package com.example.lifestyleapp
 
+import com.lifestyleapp.R
+
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
@@ -11,11 +13,16 @@ import org.junit.Rule;
 import androidx.test.rule.ActivityTestRule
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 
-import com.example.lifestyleapp.R
+import androidx.test.espresso.Espresso
 
 import androidx.test.espresso.Espresso.*
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import com.lifestyleapp.MasterDetail
+import org.hamcrest.Matchers.not
+import org.hamcrest.core.StringContains.containsString
 
 
 @RunWith(AndroidJUnit4::class)
@@ -25,8 +32,8 @@ import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
  */
 class CorrectOrderTest {
     @Rule @JvmField
-    public var activityActivityTestRule: ActivityTestRule<com.lifestyleapp.MasterDetail> =
-        ActivityTestRule<com.lifestyleapp.MasterDetail>(com.lifestyleapp.MasterDetail::class.java)
+    public var activityActivityTestRule: ActivityTestRule<MasterDetail> =
+        ActivityTestRule<MasterDetail>(MasterDetail::class.java)
 
     @Test
     fun useAppContext() {
@@ -35,111 +42,105 @@ class CorrectOrderTest {
         assertEquals("com.lifestyleapp", appContext.packageName)
     }
 
-//    @Test
-//    fun clickButtonHome() {
-//        Espresso.onView(withId(R.id.navigation_home)).perform(Espresso.click())
-//            .check(Espresso.matches(isDisplayed()))
-//    }
-
     @Test
-    fun checkOpenProfile() {
+    fun updateProfile() {
         onView(withId(R.id.my_prof_btn_frag))
-            .perform(click())
             .check(matches(isDisplayed()))
-    }
+            .perform(click())
+            .check(doesNotExist())
 
-    @Test
-    fun checkName() {
-        onView(withId(R.id.profileNameFrag))
-            .perform(typeText("NameTest"))
-            .check(matches(withText("NameTest")))
-    }
+        onView(withHint("Name:"))
+            .perform(scrollTo())
+            .perform(typeText("TestName"))
+            .check(matches(withText("TestName")))
 
-    @Test
-    fun checkAge() {
-        onView(withId(R.id.profileAgeFrag))
+        onView(withHint("Age:"))
+            .perform(scrollTo())
             .perform(typeText("20"))
             .check(matches(withText("20")))
-    }
 
-    @Test
-    fun checkCity() {
-        onView(withId(R.id.profileCityFrag))
+        onView(withHint("City:"))
+            .perform(scrollTo())
             .perform(typeText("Salt Lake City"))
             .check(matches(withText("Salt Lake City")))
-    }
 
-    @Test
-    fun checkCountry() {
-        onView(withId(R.id.profileCountryFrag))
+        onView(withHint("Country:"))
+            .perform(scrollTo())
             .perform(typeText("USA"))
             .check(matches(withText("USA")))
-    }
 
-    @Test
-    fun checkGenderBtn() {
         onView(withId(R.id.profileMaleFrag))
-            .perform(click())
+            .perform(scrollTo())
             .check(matches(isDisplayed()))
-    }
+            .perform(click())
+            .check(matches(isEnabled()))
 
-    @Test
-    fun checkSaveBtn() {
         onView(withId(R.id.saveProfileFrag))
-            .perform(click())
+            .perform(scrollTo())
             .check(matches(isDisplayed()))
-    }
+            .perform(click())
 
-    @Test
-    fun checkProfileBackBtn() {
         onView(withId(R.id.lifeBtnMyProfFrag))
-            .perform(click())
+            .perform(scrollTo())
             .check(matches(isDisplayed()))
+            .perform(click())
+            .check(doesNotExist())
     }
 
     @Test
     fun checkWeightMgt() {
         //Enter Weight Management
         onView(withId(R.id.weight_man_btn_frag))
-            .perform(click())
             .check(matches(isDisplayed()))
+            .perform(click())
+            .check(doesNotExist()) //check that the next View is displayed (onView with Id
 
         //Sedentary Btn
         onView(withId(R.id.calculatorSedentaryFrag))
+            .check(matches(isDisplayed()))
             .perform(click())
-            .check(matches(onDisplay()))
+            .check(matches(isEnabled()))
 
         //Back Btn
         onView(withId(R.id.lifestyle_btn_weightman_frag))
-            .perform(click())
             .check(matches(isDisplayed()))
+            .perform(click())
+            .check(doesNotExist())
     }
 
     @Test
     fun checkNearbyHikes() {
         //Enter Hikes
         onView(withId(R.id.hike_btn_frag))
-            .perform(click())
             .check(matches(isDisplayed()))
+//            .perform(click())
+//            .check(matches(not(isDisplayed())))
 
         //Go back
-        Espresso.pressBack()
+//        Espresso.pressBack()
+//            .perform(ViewActivity.pressBack())
     }
 
     @Test
     fun checkLocalWeather() {
         //Enter Weather
         onView(withId(R.id.weather_btn_frag))
-            .perform(click())
             .check(matches(isDisplayed()))
+            .perform(click())
+            .check(doesNotExist())
 
         //Press "Go" Btn
-        onView(withId(R.id.resetLocation))
+        onView(withText("GO"))
+            .perform(scrollTo())
             .perform(click())
             .check(matches(isDisplayed()))
 
-        //Go back
-        Espresso.pressBack()
+        //Press "Back" Btn
+        onView(withText("BACK"))
+            .perform(scrollTo())
+            .check(matches(isDisplayed()))
+            .perform(click())
+            .check(doesNotExist())
     }
 }
 
@@ -147,10 +148,10 @@ class CorrectOrderTest {
 /**
  * Tests the UI in incorrect order of use
  */
-@class IncorrectOrderTest {
+class IncorrectOrderTest {
     @Rule @JvmField
-    public var activityActivityTestRule: ActivityTestRule<com.lifestyleapp.MasterDetail> =
-        ActivityTestRule<com.lifestyleapp.MasterDetail>(com.lifestyleapp.MasterDetail::class.java)
+    public var activityActivityTestRule: ActivityTestRule<MasterDetail> =
+        ActivityTestRule<MasterDetail>(MasterDetail::class.java)
 
     @Test
     fun useAppContext() {
@@ -163,71 +164,90 @@ class CorrectOrderTest {
     fun checkLocalWeather() {
         //Enter Weather
         onView(withId(R.id.weather_btn_frag))
+            .check(matches(isDisplayed()))
+            .perform(click())
+            .check(doesNotExist())
+
+        //Press "Go" Btn
+        onView(withText("GO"))
             .perform(click())
             .check(matches(isDisplayed()))
 
-        //Press "Go" Btn
-        onView(withId(R.id.resetLocation))
+        //Press "Back" Btn
+        onView(withText("BACK"))
+            .check(matches(isDisplayed()))
             .perform(click())
-            .check(matches("Please enter a location in '[City], [Country Abbreviation] format."))
-
-        //Go back
-        Espresso.pressBack()
+            .check(doesNotExist())
     }
 
     @Test
     fun checkProfileNoInputs() {
         //Enter Profile
         onView(withId(R.id.my_prof_btn_frag))
-            .perform(click())
             .check(matches(isDisplayed()))
-
-        //Press "Save"
-        onView(withId(R.id.saveProfileFrag))
             .perform(click())
-            .check(matches("Please fill out all fields!"))
+            .check(doesNotExist())
 
-        //Go back
-        onView(withId(R.id.lifeBtnMyProfFrag))
-            .perform(click())
+        //Press "Save" Btn
+        onView(withText("Save"))
+            .perform(scrollTo())
             .check(matches(isDisplayed()))
+            .perform(click())
+//            .check(matches(withText("Please fill out all fields!")))
+
+        //Press "Back" Btn
+        onView(withText("Back"))
+            .perform(scrollTo())
+            .check(matches(isDisplayed()))
+            .perform(click())
+            .check(doesNotExist())
     }
 
     @Test
     fun checkProfilePartialInputs() {
         //Enter Profile
         onView(withId(R.id.my_prof_btn_frag))
-            .perform(click())
             .check(matches(isDisplayed()))
+            .perform(click())
+            .check(doesNotExist())
 
         //Enter Name
         onView(withId(R.id.profileNameFrag))
-            .perform(typeText("NameTest"))
+            .perform(scrollTo())
+            .perform(replaceText("NameTest"))
             .check(matches(withText("NameTest")))
 
         //Enter Age
         onView(withId(R.id.profileAgeFrag))
-            .perform(typeText("20"))
+            .perform(scrollTo())
+            .perform(replaceText("20"))
             .check(matches(withText("20")))
 
         //Enter City
         onView(withId(R.id.profileCityFrag))
-            .perform(typeText("Salt Lake City"))
+            .perform(scrollTo())
+            .perform(replaceText("Salt Lake City"))
             .check(matches(withText("Salt Lake City")))
 
         //Enter Country
         onView(withId(R.id.profileCountryFrag))
-            .perform(typeText("USA"))
+            .perform(scrollTo())
+            .perform(replaceText("USA"))
             .check(matches(withText("USA")))
 
-        //Press "Save"
+        //Press "Save" Btn
         onView(withId(R.id.saveProfileFrag))
-            .perform(click())
-            .check(matches("Please select a gender!"))
-
-        //Go back
-        onView(withId(R.id.lifeBtnMyProfFrag))
-            .perform(click())
+            .perform(scrollTo())
             .check(matches(isDisplayed()))
+            .perform(click())
+//            .check(matches(withText("Please select a gender!")))
+//            .check(matches(withText(containsString("Please select a gender!"))))
+
+        //Press "Back" Btn
+        onView(withId(R.id.lifeBtnMyProfFrag))
+            .perform(scrollTo())
+            .check(matches(isDisplayed()))
+            .perform(click())
+            .check(doesNotExist())
     }
 }
